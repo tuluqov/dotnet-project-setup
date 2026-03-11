89e5e83e24ec44757b0dbfc00979fb150a41c3c3
Yusup <usup1999@gmail.com>
2026-03-11 16:02:43 +0300

    Добавьте файлы проекта.

---
+++ Web.Api/Program.cs
@@ -0,0 +1,24 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+builder.Host.UseSerilog((context, configuration) => 
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
